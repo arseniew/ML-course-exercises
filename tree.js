67fc6d4a72322dc45216dc0b44d95b55aafb7220
Marcin Batkowski <arseniew@gmail.com>
2016-06-01 23:11:42 +0100

    Basic source data processing

---
+++ tree.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var _ = require('lodash');
+var rawData = fs.readFileSync('./data', 'utf-8');
+
+var indexColumn = 'index';
+var resultColumn = 'play';
+var columns = [indexColumn, 'outlook', 'temperature', 'humidity', 'wind', resultColumn];
+var attributesCColumns = _.without(columns, indexColumn, resultColumn);
+
+var data = _(rawData)
+    .split('\n')
+    .compact()
+    .map(_.partial(_.split, _, ' ', -1))
+    .value()
+var labeledData = _.zipObject(columns, _.unzip(data));
+
+console.log(
+  _.mapValues(
+    _.groupBy(_.zip(labeledData['outlook'], labeledData[resultColumn]), 0),
+    _.partial(_.countBy, _, 1)
+  )
+)
